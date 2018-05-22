9e6455d2c9d5122da6288904d2256c8682635a76
Daniel Schmidt <silveirinha@gmail.com>
2018-05-22 20:36:08 -0300

    initial commit

---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule MarsRover.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :mars_rover,
+      version: "0.0.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
