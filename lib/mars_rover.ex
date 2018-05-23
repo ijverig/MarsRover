f3042ddb9a1243a15a09e7a7b7db3dbd86c5cb4d
Daniel Schmidt <silveirinha@gmail.com>
2018-05-22 23:34:02 -0300

    added rotations

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,7 +1,20 @@
 defmodule MarsRover do
   def process(current_position, command)
+
   def process({x,y,:north}, :move), do: {x, y+1, :north}
   def process({x,y,:south}, :move), do: {x, y-1, :south}
   def process({x,y,:west}, :move), do: {x-1, y, :west}
   def process({x,y,:east}, :move), do: {x+1, y, :east}
+
+  def process({x,y,:north}, :left), do: {x, y, :west}
+  def process({x,y,:north}, :right), do: {x, y, :east}
+
+  def process({x,y,:south}, :left), do: {x, y, :east}
+  def process({x,y,:south}, :right), do: {x, y, :west}
+
+  def process({x,y,:west}, :left), do: {x, y, :south}
+  def process({x,y,:west}, :right), do: {x, y, :north}
+
+  def process({x,y,:east}, :left), do: {x, y, :north}
+  def process({x,y,:east}, :right), do: {x, y, :south}
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -14,4 +14,22 @@ defmodule MarsRoverTest do
     position_facing_east = {1,1,:east}
     assert MarsRover.process(position_facing_east, :move) == {2,1,:east}
   end
+
+  test "rotations" do
+    position_facing_north = {0,0,:north}
+    assert MarsRover.process(position_facing_north, :left) == {0,0,:west}
+    assert MarsRover.process(position_facing_north, :right) == {0,0,:east}
+
+    position_facing_south = {0,0,:south}
+    assert MarsRover.process(position_facing_south, :left) == {0,0,:east}
+    assert MarsRover.process(position_facing_south, :right) == {0,0,:west}
+
+    position_facing_west = {0,0,:west}
+    assert MarsRover.process(position_facing_west, :left) == {0,0,:south}
+    assert MarsRover.process(position_facing_west, :right) == {0,0,:north}
+
+    position_facing_east = {0,0,:east}
+    assert MarsRover.process(position_facing_east, :left) == {0,0,:north}
+    assert MarsRover.process(position_facing_east, :right) == {0,0,:south}
+  end
 end
