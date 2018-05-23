cb0fcdcdc4dc035368ad8d1aa16ef94d76a04516
Daniel Schmidt <silveirinha@gmail.com>
2018-05-22 22:17:04 -0300

    added movements

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,2 +1,7 @@
 defmodule MarsRover do
+  def process(current_position, command)
+  def process({x,y,:north}, :move), do: {x, y+1, :north}
+  def process({x,y,:south}, :move), do: {x, y-1, :south}
+  def process({x,y,:west}, :move), do: {x-1, y, :west}
+  def process({x,y,:east}, :move), do: {x+1, y, :east}
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -1,3 +1,17 @@
 defmodule MarsRoverTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  test "movements" do
+    position_facing_north = {1,1,:north}
+    assert MarsRover.process(position_facing_north, :move) == {1,2,:north}
+
+    position_facing_south = {1,1,:south} 
+    assert MarsRover.process(position_facing_south, :move) == {1,0,:south}
+    
+    position_facing_west = {1,1,:west}
+    assert MarsRover.process(position_facing_west, :move) == {0,1,:west}
+
+    position_facing_east = {1,1,:east}
+    assert MarsRover.process(position_facing_east, :move) == {2,1,:east}
+  end
 end
