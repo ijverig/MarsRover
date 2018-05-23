5939098260725d3b4431d16ed2a55fce263c2e18
Daniel Schmidt <silveirinha@gmail.com>
2018-05-23 02:18:45 -0300

    added multiple commands processing

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,2 +1,15 @@
 defmodule MarsRover do
+  import MarsRover.Controls
+
+  def process_commands(current_position, []), do: current_position
+
+  def process_commands(current_position, [current_command | remaining_commands]) do
+    current_position
+    |> process_command(current_command)
+    |> process_commands(remaining_commands)
+  end
+
+  defp process_command(current_position, :move), do: move(current_position)
+  defp process_command(current_position, :left), do: rotate_left(current_position)
+  defp process_command(current_position, :right), do: rotate_right(current_position)
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -1,3 +1,27 @@
 defmodule MarsRoverTest do
   use ExUnit.Case, async: true
+
+  import MarsRover
+
+  test "rotate 180 degress" do
+    position_facing_north = {0, 0, :north}
+    assert process_commands(position_facing_north, [:left, :left]) == {0, 0, :south}
+    assert process_commands(position_facing_north, [:right, :right]) == {0, 0, :south}
+  end
+
+  test "rotate 270 degress" do
+    position_facing_south = {0, 0, :south}
+    assert process_commands(position_facing_south, [:left, :left, :left]) == {0, 0, :west}
+    assert process_commands(position_facing_south, [:right, :right, :right]) == {0, 0, :east}
+  end
+
+  test "move from 0,0,N to 3,2,S" do
+    commands = ~W/right move move left move move move right move right move/a
+    assert process_commands({0, 0, :north}, commands) == {3, 2, :south}
+  end
+
+  test "move from 3,2,S to 0,0,N" do
+    commands = ~W/left left move left move left move move move right move move right/a
+    assert process_commands({3, 2, :south}, commands) == {0, 0, :north}
+  end
 end
