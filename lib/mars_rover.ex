54d71f7889c0a4cf4032f07b4a72203d6c15f77d
Daniel Schmidt <silveirinha@gmail.com>
2018-05-24 01:02:38 -0300

    moved deploy stuff to Deployments

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,27 +1,8 @@
 defmodule MarsRover do
-  import MarsRover.{Controls, Plateau}
-
-  def process_deployments(plateau, deployments) do
-    Enum.map(deployments, fn {initial_position, commands} ->
-      run_commands(initial_position, plateau, commands)
-    end)
-  end
-
-  def run_commands({x, y, _heading}, {max_x, max_y}, _commands)
-      when is_off_plateau(x, y, max_x, max_y),
-      do: {:error, :off_plateau}
-
-  def run_commands(current_position, _plateau, []), do: {:ok, current_position}
-
-  def run_commands(current_position, plateau, [current_command | remaining_commands]) do
-    current_position
-    |> run_command(current_command)
-    |> run_commands(plateau, remaining_commands)
-  end
-
-  defp run_command(current_position, :move), do: move(current_position)
-  defp run_command(current_position, :left), do: rotate_left(current_position)
-  defp run_command(current_position, :right), do: rotate_right(current_position)
+  alias MarsRover.Deployments
+  
+  def process_deployments(plateau, deployments),
+    do: Deployments.process_deployments(plateau, deployments)
 
   def version do
     {:ok, version} = :application.get_key(:mars_rover, :vsn)
