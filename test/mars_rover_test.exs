9dfdb08a5e7b9102fb0b4db6ad43b62f670f32b2
Daniel Schmidt <silveirinha@gmail.com>
2018-05-27 14:58:57 -0300

    added a 0by0 plateau test

--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -1,6 +1,22 @@
 defmodule MarsRoverTest do
   use ExUnit.Case, async: false
 
+  test "deployments on 0by0 plateau" do
+    plateau = {0, 0}
+
+    deployments = [
+      {{0, 0, :north}, [:move]},
+      {{0, 0, :east}, [:right]},
+      {{4, 3, :west}, [:left]}
+    ]
+
+    assert MarsRover.deploy(plateau, deployments) == [
+             {:error, :off_plateau},
+             {:ok, {0, 0, :south}},
+             {:error, :off_plateau}
+           ]
+  end
+
   test "processing several deployments" do
     plateau = {1, 1}
 
