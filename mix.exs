c82b9031430e431745aef06b0596b81fcf2467a3
Daniel Schmidt <silveirinha@gmail.com>
2018-05-28 20:22:42 -0300

    bumped to v2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MarsRover.MixProject do
   def project do
     [
       app: :mars_rover,
-      version: "2.1.0",
+      version: "2.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
